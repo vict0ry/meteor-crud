b209936ccaeb539d24279da409522346c6a889bf
Lezhnev Victor <lezhnev.victor@gmail.com>
2016-07-19 15:23:02 +0200

    save time issues
    fixed issues while save time

--- client/Tasks/tasks.js
+++ client/Tasks/tasks.js
@@ -85,7 +85,7 @@ Template.task.events({
           //
           // removeAllRelatedToTask(this._id);
 
-          
+
           //remove all related to this task :
           Meteor.call("removeAllRelatedToTask",this._id);
       }
@@ -106,17 +106,6 @@ Template.task.events({
     },
     'keypress .edit' (event) {
         if (event.which == 13) {
-          // historyTasks = TasksHistory.find({
-          //   parentId:'FsgCKFoMkAzezyqw9'
-          // });
-          // historyTasks.forEach(function(row){
-          //   TasksHistory.remove({_id:row._id});
-          //   console.log(row._id);
-          // });
-          // console.log(history);
-          // call function that will recalculate time
-          //test
-          // console.log(this);
 
             event.preventDefault();
             whatClass = event.target.classList[0];
@@ -131,7 +120,6 @@ Template.task.events({
             whatToChange = {
                 [nameIt]: whatValue // change property name in object
             };
-            // console.log(this._id);
             // remove it
             recount = Tasks.findOne({
               _id: this._id
@@ -145,18 +133,6 @@ Template.task.events({
                 differenceIntern: ((recount.worked - ((recount.ratio / 100) * recount.worked)) - recount.estimation).toFixed(2)
             }; //count intern difference
 
-
-
-
-            // task = UserTimes.findOne({
-            //   _id: this._id
-            // });
-            // //test//
-            // //test//
-            // task.estimation - this.estimation;
-            //
-
-
             this.estimation = whatValue;
             Meteor.call("updateWorkedTime",this._id,this);
 
@@ -168,49 +144,6 @@ Template.task.events({
               hRatio = this.ratio - getFromCollection.ratio;
 
               Meteor.call('createHistory',hEstimation,hWorked,hRatio,this);
-
-
-
-            //
-            // TasksHistory.insert({
-            //   parentId : this._id,
-            //   estimation: this.estimation,
-            //   user: Meteor.user().username
-            // });
-
-
-
-/******************* COUNT DIFFERENCE AND DIFFERENCE INTERN AFTER PRESS ENTER ****************************/
-            // allTogetherWorked = 0;
-            // allTogetherWorkedMinusRatio = 0;
-            //
-            // userTimes = UserTimes.find({
-            //   parentId: this._id
-            // });
-            // userTimes.forEach(function(row){
-            //   ratio = row.ratio / 100;
-            //   // console.log(ratio);
-            //   workedMinusRatio = row.worked * ratio;
-            //   allTogetherWorkedMinusRatio += row.worked - workedMinusRatio;
-            //   allTogetherWorked += row.worked;
-            // });
-            //
-            // newDifference = allTogetherWorked - this.estimation;
-            // newDifferenceIntern = allTogetherWorkedMinusRatio - this.estimation;
-            // Tasks.update(this._id, {
-            //     $set: {
-            //         difference: newDifference,
-            //         differenceIntern: newDifferenceIntern,
-            //         worked: allTogetherWorked,
-            //         workedIntern : allTogetherWorkedMinusRatio
-            //     }
-            // });
-/******************* END COUNT DIFFERENCE AND DIFFERENCE INTERN AFTER PRESS ENTER ****************************/
-
-
-
-
-
             /***************  update contenteditable) ****************/
             Meteor.call("updateAnything", this._id, whatToChange);
 
